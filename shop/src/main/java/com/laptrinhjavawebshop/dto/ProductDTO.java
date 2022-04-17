a99e63a73d8f819707eaa35228e3a6fd5f13f1b9
trung hoang <trung.hoangquoc203@gmail.com>
2022-04-18 02:32:26 +0700

    commit Create Product

---
+++ shop/src/main/java/com/laptrinhjavawebshop/dto/ProductDTO.java
@@ -0,0 +1,46 @@
+package com.laptrinhjavawebshop.dto;
+
+public class ProductDTO extends AbstractDTO<ProductDTO> {
+	private String name;
+	private Long totalNumber;
+	private String shortDescription;
+	private Float price;
+	private Long categoryId;
+	private String categoryCode;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Long getTotalNumber() {
+		return totalNumber;
+	}
+	public void setTotalNumber(Long totalNumber) {
+		this.totalNumber = totalNumber;
+	}
+	public String getShortDescription() {
+		return shortDescription;
+	}
+	public void setShortDescription(String shortDescription) {
+		this.shortDescription = shortDescription;
+	}
+	public Long getCategoryId() {
+		return categoryId;
+	}
+	public void setCategoryId(Long categoryId) {
+		this.categoryId = categoryId;
+	}
+	public String getCategoryCode() {
+		return categoryCode;
+	}
+	public void setCategoryCode(String categoryCode) {
+		this.categoryCode = categoryCode;
+	}
+	public Float getPrice() {
+		return price;
+	}
+	public void setPrice(Float price) {
+		this.price = price;
+	}
+}
---
+++ shop/src/main/java/com/laptrinhjavawebshop/entity/CategoryEntity.java
@@ -0,0 +1,52 @@
+package com.laptrinhjavawebshop.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+
+
+@Entity
+@Table(name = "category")
+public class CategoryEntity extends BaseEntity {
+
+	@Column(name = "name")
+	private String name;
+
+	@Column(name = "code")
+	private String code;
+
+	@OneToMany(mappedBy = "category")
+	private List<ProductEntity> products = new ArrayList<>();
+	//vì 1 category có nhiều products nên cần tạo 1 list
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCode() {
+		return code;
+	}
+
+	public void setCode(String code) {
+		this.code = code;
+	}
+
+	public List<ProductEntity> getProducts() {
+		return products;
+	}
+
+	public void setProducts(List<ProductEntity> products) {
+		this.products = products;
+	}
+
+
+}
